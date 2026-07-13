7af352d909a11cfe46a6ef4d2b0caa95be6e878c
113090ab-creator <113090ab@gmail.com>
2026-07-13 15:06:37 +0900

    1

--- open_dashboard.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-set "DASHBOARD_URL=https://interojo-domestic.streamlit.app/"
-
-start "" "%DASHBOARD_URL%"
-echo Dashboard opened: %DASHBOARD_URL%
-
-endlocal
--- stop_dashboard.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal EnableExtensions
-cd /d "%~dp0"
-
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0stop_dashboard.ps1"
-exit /b %errorlevel%
